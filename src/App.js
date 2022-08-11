e0d22435d3e3039192e45bf45eefc9817273367e
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2022-08-11 14:59:43 +0300

    zamanlayıcı uygulaması yapıldı arttırmak/eksiltmek butonları eklendi. 600 dk ya sabitlendi

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import DigitalTimer from './components/DigitalTimer'
+
+import './App.css'
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+        <div><DigitalTimer /></div>
+        <div>Kripto para birimleri buraya gelecek</div>
+    </div>
+  )
+}
+
+export default App
